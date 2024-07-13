145391c3a6c64099fb508642e001e105f50776a5
developer-tamim <tawsiftamim24@gmail.com>
2024-07-14 00:15:13 +0600

    dashboard import

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SchoolDashboard;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,3 +30,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('school_dashboard',[SchoolDashboard::class, 'index'])->name('school.dashboard');
