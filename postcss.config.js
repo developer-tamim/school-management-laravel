e46511daaafa4528a63309639d740036d2710026
developer-tamim <tawsiftamim24@gmail.com>
2024-07-27 21:11:51 +0600

    drop old and import new project

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-// export default {
-    module.exports= {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    },
-};
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\SchoolDashboard;
-use App\Http\Controllers\TeachersController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,26 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::get('/', function () {
+    return view('welcome');
 });
-
-require __DIR__.'/auth.php';
-
-// Route::get('school_dashboard',[SchoolDashboard::class, 'index'])->name('school.dashboard');
-Route::get('/',[SchoolDashboard::class, 'index'])->name('school.dashboard');
-
-
-// Teacher routes
-Route::get('teachers',[TeachersController::class, 'index'])->name('teacher.index');
-Route::get('teachers/add',[TeachersController::class, 'create'])->name('teacher.add');
